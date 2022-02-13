ceda4adc1bdc4902c990bfca65b205798ba666cb
Brian Holt <btholt@gmail.com>
2022-02-13 15:19:47 -0800

    portals, refs, and updated styles

---
+++ 13-portals-and-refs/src/Details.js
@@ -0,0 +1,53 @@
+import { Suspense, useState } from "react";
+import { useParams } from "react-router-dom";
+import getPet from "./getPet";
+import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
+import Modal from "./Modal";
+
+function DetailsParent() {
+  const { id } = useParams();
+  const resource = getPet(id);
+
+  return (
+    <Suspense fallback={<h2>loading …</h2>}>
+      <Details resource={resource} />
+    </Suspense>
+  );
+}
+
+const Details = ({ resource }) => {
+  const pet = resource.readData();
+  const [showModal, setShowModal] = useState(false);
+
+  return (
+    <div className="details">
+      <Carousel images={pet.images} />
+      <div>
+        <h1>{pet.name}</h1>
+        <h2>{`${pet.animal} — ${pet.breed} — ${pet.city}, ${pet.state}`}</h2>
+        <button onClick={() => setShowModal(true)}>Adopt {pet.name}</button>
+        <p>{pet.description}</p>
+        {showModal ? (
+          <Modal>
+            <div>
+              <h1>Would you like to adopt {pet.name}?</h1>
+              <div className="buttons">
+                <a href="https://bit.ly/pet-adopt">Yes</a>
+                <button onClick={() => setShowModal(false)}>No</button>
+              </div>
+            </div>
+          </Modal>
+        ) : null}
+      </div>
+    </div>
+  );
+};
+
+export default function DetailsErrorBoundary(props) {
+  return (
+    <ErrorBoundary>
+      <DetailsParent {...props} />
+    </ErrorBoundary>
+  );
+}
